c83ac471a14cc17ca405a8602f64a7050166f7d8
MediciPrime <behram11@gmail.com>
2017-06-30 16:49:21 -0500

    Add List model to insert list names w/respect to items.

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class List(models.Model):
+    pass
+
 # TODO: Support more than one list!
 class Item(models.Model):
     text = models.TextField(default='ignore')
+    list = models.ForeignKey(List, default=None)
